2cb81781b0d7878a35bf2bbe1c4cdc255584f151
Kayton <kaytoncwc@gmail.com>
2024-07-14 16:13:10 +0800

    game: added rule instruction

--- reversi.go
+++ reversi.go
@@ -104,6 +104,12 @@ func userMove(game *Game) {
 func main() {
 	// entry point of the game
 	// the game will be running as a loop
+	fmt.Println(`[Game Rule]
+1. must be adjacent to another opponent chess
+2. must form a straight line with another chess of yours (straight or tilted)
+3. the opponent chess in between your chess and your new move will be flipped
+	`)
+
 	game := initGame()
 	for {
 		userMove(&game)
